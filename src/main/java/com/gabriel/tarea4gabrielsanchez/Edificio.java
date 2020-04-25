052a8e7050281cdb5a9fe905b06a1b4163f937d5
Gabriel <gabionezero@gmail.com>
2020-04-25 19:24:29 +0200

    Añade getters y setters en clase modelo

--- src/main/java/com/gabriel/tarea4gabrielsanchez/Edificio.java
+++ src/main/java/com/gabriel/tarea4gabrielsanchez/Edificio.java
@@ -23,6 +23,30 @@ public class Edificio {
         this.numeroPlantas = numeroPlantas;
         this.tieneAscensor = tieneAscensor;
     }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public int getNumeroPlantas() {
+        return numeroPlantas;
+    }
+
+    public void setNumeroPlantas(int numeroPlantas) {
+        this.numeroPlantas = numeroPlantas;
+    }
+
+    public boolean isTieneAscensor() {
+        return tieneAscensor;
+    }
+
+    public void setTieneAscensor(boolean tieneAscensor) {
+        this.tieneAscensor = tieneAscensor;
+    }
    
     
     
